307e2cfe917948ce2a408d14789139b169b5566d
Nish Shyam <nshyam668@gmail.com>
2020-01-31 19:31:04 +0000

    first commit

---
+++ train.py
@@ -0,0 +1,87 @@
+import itertools
+import pandas
+import numpy
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from pylab import rcParams
+
+data = pandas.read_csv('train.csv')
+
+print(data.head())
+
+first = data[data['ATM_ID'] == 0].copy()
+
+first.loc[:, 'DATE'] = pandas.to_datetime(first['DATE'])
+print(first['DATE'].max() - first['DATE'].min())
+
+first = first.set_index('DATE')
+y = first['CLIENT_OUT'].resample('MS').mean()
+
+# plt.plot(y)
+# plt.show()
+
+# rcParams['figure.figsize'] = 18, 8
+# decomposition = sm.tsa.seasonal_decompose(y, model='additive')
+# fig = decomposition.plot()
+# plt.show()
+
+p = d = q = range(0, 2)
+pdq = list(itertools.product(p, d, q))
+seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
+print('Examples of parameter combinations for Seasonal ARIMA...')
+print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[1]))
+print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[2]))
+print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[3]))
+print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[4]))
+
+# for param in pdq:
+#     for param_seasonal in seasonal_pdq:
+#         try:
+#             mod = sm.tsa.statespace.SARIMAX(y,
+#                                             order=param,
+#                                             seasonal_order=param_seasonal,
+#                                             enforce_stationarity=False,
+#                                             enforce_invertibility=False)
+#             results = mod.fit()
+#             print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
+#         except:
+#             continue
+
+mod = sm.tsa.statespace.SARIMAX(y,
+                                order=(1, 1, 1),
+                                seasonal_order=(1, 1, 1, 12),
+                                enforce_stationarity=False,
+                                enforce_invertibility=False)
+results = mod.fit()
+print(results.summary().tables[1])
+
+pred = results.get_prediction(start=pandas.to_datetime('2017-01-01'), dynamic=False)
+pred_ci = pred.conf_int()
+ax = y['2015':].plot(label='observed')
+pred.predicted_mean.plot(ax=ax, label='One-step ahead Forecast', alpha=.7, figsize=(14, 7))
+ax.fill_between(pred_ci.index,
+                pred_ci.iloc[:, 0],
+                pred_ci.iloc[:, 1], color='k', alpha=.2)
+ax.set_xlabel('Date')
+ax.set_ylabel('CLIENT_OUT')
+plt.legend()
+# plt.show()
+
+y_forecasted = pred.predicted_mean
+y_truth = y['2017-01-01':]
+mse = ((y_forecasted - y_truth) ** 2).mean()
+print('The Mean Squared Error of our forecasts is {}'.format(round(mse, 2)))
+
+print('The Root Mean Squared Error of our forecasts is {}'.format(round(numpy.sqrt(mse), 2)))
+
+pred_uc = results.get_forecast(steps=100)
+pred_ci = pred_uc.conf_int()
+ax = y.plot(label='observed', figsize=(14, 7))
+pred_uc.predicted_mean.plot(ax=ax, label='Forecast')
+ax.fill_between(pred_ci.index,
+                pred_ci.iloc[:, 0],
+                pred_ci.iloc[:, 1], color='k', alpha=.25)
+ax.set_xlabel('Date')
+ax.set_ylabel('CLIENT_OUT')
+plt.legend()
+plt.show()
